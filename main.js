9abc869d329ef1004a7f0eda8e12906600b2c32e
Josephina <jyergatianmurfitt@googlemail.com>
2021-08-27 14:33:30 +0100

    added styles

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = () => {
   var tryCount = 0;
   var matchCount = 0;
 
+//Display scoreboard count
   var tryCountDisplay = document.querySelector('.tryCount');
   tryCountDisplay.textContent = tryCount;
   var matchCountDisplay = document.querySelector('.matchCount');
@@ -15,14 +16,18 @@ const app = () => {
 
       if (openCards.length <= 2 ) {
         if(openCards.length < 2) {
+          //Check if same card clicked twice:
           if (openCards.length === 1 && openCards[0].id === card.id) {
             alert('Same card clicked twice');
+          //Chech if matched card was clicked:
           } else if (card.classList.contains('matched')) {
             alert('Card already matched');
+          //Open new card
           }else if (openCards.length === 1 && openCards[0].id !== card.id || openCards.length === 0) {
             card.classList.add('open');
             openCards.push(card);
             tryCount++
+            //Close chosen cards after 2nd card choice
             if(openCards.length === 2 && openCards[0].className !== openCards[1].className) {
               setTimeout(function() {
                 openCards.forEach(function (openCard) {
@@ -30,6 +35,7 @@ const app = () => {
                   openCards = [];
                 });
               }, 1000);
+            //Match cards
             } else if (openCards.length === 2 && openCards[0].className == openCards[1].className) {
               openCards.forEach(function (matchedCard) {
                 matchedCard.classList.remove('open');
@@ -41,6 +47,8 @@ const app = () => {
           }
         }
       }
+
+      //Update scoreboard count
       matchCountDisplay.textContent = matchCount / 2;
       if(tryCount % 2 == 0) {
         tryCountDisplay.textContent = tryCount / 2;
